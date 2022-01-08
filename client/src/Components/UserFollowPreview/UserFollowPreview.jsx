d7524c6fd680893fd5499f3a6f8cf7be8c43bfb6
Joshua <joshuatauro45@gmail.com>
2022-01-08 23:44:12 +0530

    Added hover states and icons

--- client/src/Components/UserFollowPreview/UserFollowPreview.jsx
+++ client/src/Components/UserFollowPreview/UserFollowPreview.jsx
@@ -1,23 +1,26 @@
 import { ExternalLinkIcon } from '@heroicons/react/outline'
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './UserFollowPreview.css'
 const UserFollowPreview = () => {
   return (
     <article className="user-preview">
-      <div className="user-preview__container">
-        <div className="user-preview__flex icon">
-          <div className="user-preview__flex ">
-            <img className='user-preview__avatar' src="https://dogesocial.vercel.app/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdogecorp%2Fimage%2Fupload%2Fv1631713031%2Fdogesocial%2Fv1%2Fimages%2F12_k69qix.svg&w=1920&q=75" alt="" />
-            <div className="user-preview__flex col">
-              <div className="user-preview__col ">
-                <h1 className='user-preview__name'>Joshua Tauro </h1>
-                <h2 className='user-preview__username'>@joshuatauro45</h2>
+      <Link to={`/user/joshuatauro45`}>
+        <div className="user-preview__container">
+          <div className="user-preview__flex icon">
+            <div className="user-preview__flex ">
+              <img className='user-preview__avatar' src="https://dogesocial.vercel.app/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdogecorp%2Fimage%2Fupload%2Fv1631713031%2Fdogesocial%2Fv1%2Fimages%2F12_k69qix.svg&w=1920&q=75" alt="" />
+              <div className="user-preview__flex col">
+                <div className="user-preview__col ">
+                  <h1 className='user-preview__name'>Joshua Tauro </h1>
+                  <h2 className='user-preview__username'>@joshuatauro45</h2>
+                </div>
               </div>
             </div>
+            <ExternalLinkIcon height={20} color="white" />
           </div>
-          <ExternalLinkIcon height={20} />
         </div>
-      </div>
+      </Link>
     </article>
   )
 }
--- client/src/Pages/Landing/Landing.jsx
+++ client/src/Pages/Landing/Landing.jsx
@@ -1,4 +1,6 @@
+import { GlobeIcon, HomeIcon, UsersIcon } from '@heroicons/react/outline'
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Post from '../../Components/Post/Post'
 import UserFollowPreview from '../../Components/UserFollowPreview/UserFollowPreview'
 import './Landing.css'
@@ -7,7 +9,49 @@ const Landing = () => {
     <section className="landing">
       <div className="landing__container">
         <div className="landing__grid">
-          <div className="landing__col">1</div>
+        <div className="landing__col col-3">
+            <h1 className='landing__title landing__title-3'>Navigation</h1>
+            <div className="user-preview-col">
+              <div className="user-preview-col__container">
+                <article className="nav-link active">
+                  <Link to="/">
+                    <div className="nav-link__container">
+                      <div className="nav-link__flex">
+                        <HomeIcon width={20} />
+                        <h1 className="nav-link__title">
+                          Your feed
+                        </h1>
+                      </div>
+                    </div>
+                  </Link>
+                </article>
+                <article className="nav-link">
+                  <Link to="/">
+                  <div className="nav-link__container">
+                      <div className="nav-link__flex">
+                        <GlobeIcon width={20} />
+                        <h1 className="nav-link__title">
+                          All
+                        </h1>
+                      </div>
+                    </div>
+                  </Link>
+                </article>
+                <article className="nav-link">
+                  <Link to="/">
+                  <div className="nav-link__container">
+                      <div className="nav-link__flex">
+                        <UsersIcon width={20} />
+                        <h1 className="nav-link__title">
+                          Users
+                        </h1>
+                      </div>
+                    </div>
+                  </Link>
+                </article>
+              </div>
+            </div>
+          </div>
 
           <div className="landing__col">
             <div className="landing__flex">
@@ -22,7 +66,7 @@ const Landing = () => {
           </div>
 
           <div className="landing__col col-3">
-            <h1 className='landing__title landing__title-3'>People to follow</h1>
+            <h1 className='landing__title landing__title-3'>People to checkout</h1>
             <div className="user-preview-col">
               <div className="user-preview-col__container">
                 <UserFollowPreview />
